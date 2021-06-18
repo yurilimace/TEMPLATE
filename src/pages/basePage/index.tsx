91b8537f00578fc40106f678f1ebab9d2497a43a
Moisés Mariano <moisesduartem@gmail.com>
2021-06-18 16:10:37 -0300

    Validação com date + input + textarea

--- src/pages/basePage/index.tsx
+++ src/pages/basePage/index.tsx
@@ -7,11 +7,22 @@ import { FormHandles } from '@unform/core'
 
 const BasePage = () => {
   const formRef =  useRef<FormHandles>(null)
-  const handleSubmit = (data: { input: string }) =>{
+  const handleSubmit = (data: { textarea: string; dateInput: Date | string; input: string }) =>{
+
+    formRef.current?.setErrors({})
+
     console.log(data)
+
+    if (data.textarea) {
+      formRef.current?.setFieldError('textarea','deu ruim')
+    }
+
     if(data.input === ''){
       formRef.current?.setFieldError('input','deu ruim')
-      formRef.current?.setFieldError('textarea','deu ruim')
+    }
+
+    if (data.dateInput === "") {
+      formRef.current?.setFieldError('dateInput','deu ruim a data')
     }
   }
 
@@ -22,7 +33,7 @@ const BasePage = () => {
         <TextArea name="textarea" label="textarea" className="w-50 mt-3 mb-3" linhas={2}  />
         <div className="d-flex flex-column w-100">
         <Input name="input" label="input" className="w-50" />
-        <Input name="input" type="date" label="datas" className="w-50" />
+        <Input name="dateInput" type="date" label="datas" className="w-50" />
         <SelectComponent/>
         </div>
         <button type="submit" > click me </button>
