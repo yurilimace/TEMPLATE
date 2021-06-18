8141d5f4c55d092477058bee55953c043f7c8d12
Moisés Mariano <moisesduartem@gmail.com>
2021-06-18 08:37:17 -0300

    fix: store.tsx -> store/index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import App from './App';
 // import { icons } from './assets/icons'
 
 import { Provider } from 'react-redux'
-import store from './store'
+import store from './store/'
 
 ReactDOM.render(
   <Provider store={store}>
